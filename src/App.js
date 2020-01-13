e4f08ef87f5c02cb7addca0bdd00de21ceb020bf
Matt Bowen <mattbowen@Matts-MBP.fios-router.home>
2020-01-12 21:27:15 -0500

    initial commit for burger-builder

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from './components/Layout/Layout';
+function App() {
+  return (
+    <div>
+      <Layout>
+        <p>Test</p>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import Aux from '../../hoc/Aux';
+
+const layout = props => (
+  <Aux>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main>{props.children}</main>
+  </Aux>
+);
+
+export default layout;
