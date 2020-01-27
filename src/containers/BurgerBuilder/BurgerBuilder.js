1819e865ec73ac2896abd9d326c3d4275fdcf320
Matt Bowen <bowen9284@gmail.com>
2020-01-26 21:08:31 -0500

    updating file structure, moving layout into hoc directory since it wraps the application

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
@@ -69,7 +69,7 @@ class BurgerBuilder extends Component {
       updatedIngredients[type] = count - 1;
       this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
     }
-    
+
     this.updatePurchaseState(updatedIngredients);
   };
 
