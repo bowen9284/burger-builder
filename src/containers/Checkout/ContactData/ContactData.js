846613aaa21013b10c21a635bd1a67df5f1f80a3
Matt Bowen <bowen9284@gmail.com>
2020-02-23 22:14:05 -0500

    creating component to handle form inputs

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 const ContactData = props => {
   const [contactInfo, setContactInfo] = useState({
@@ -47,30 +48,10 @@ const ContactData = props => {
 
   let form = (
     <form>
-      <input
-        className={classes.Input}
-        type="email"
-        name="email"
-        placeholder="Your Email"
-      />
-      <input
-        className={classes.Input}
-        type="text"
-        name="name"
-        placeholder="Your Name"
-      />
-      <input
-        className={classes.Input}
-        type="text"
-        name="street"
-        placeholder="Your Street"
-      />
-      <input
-        className={classes.Input}
-        type="text"
-        name="zipCode"
-        placeholder="Your Zip Code"
-      />
+      <Input inputtype="input" type="email" name="email" placeholder="Your Email" />
+      <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
+      <Input inputtype="input" type="text" name="street" placeholder="Your Street" />
+      <Input inputtype="input" type="text" name="zipCode" placeholder="Your Zip Code" />
       <Button btnType="Success" clicked={orderHandler}>
         Submit
       </Button>
