b726c99765e548a00be5a1dc5662e6174e1ac0fd
Matt Bowen <bowen9284@gmail.com>
2020-02-25 19:04:24 -0500

    adding select and fixing bug where I passed type instead of elementType in elementType prop

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -23,6 +23,15 @@ const Input = props => {
         />
       );
       break;
+    case 'select':
+      inputElement = (
+        <select className={classes.InputElement} value={props.value}>
+          {props.elementConfig.options.map(option => (
+            <option value={option.value}>{option.displayValue}</option>
+          ))}
+        </select>
+      );
+      break;
     default:
       inputElement = (
         <input
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -93,7 +93,7 @@ const ContactData = props => {
       {formElementsArray.map(el => (
         <Input
           key={el.id}
-          elementType={el.config.type}
+          elementType={el.config.elementType}
           elementConfig={el.config.elementConfig}
           value={el.config.value}
         />
