31e9e19fc8a66804e68b9426120acc1c076cc743
Matt Bowen <bowen9284@gmail.com>
2020-06-16 21:38:52 -0400

    adding authentication

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -4,8 +4,6 @@ export {
   initIngredients,
 } from './burgerBuilder';
 
-export {
-  purchaseBurger,
-  purchaseInit,
-  fetchOrders,
-} from './order';
+export { purchaseBurger, purchaseInit, fetchOrders } from './order';
+
+export { auth } from './auth';
