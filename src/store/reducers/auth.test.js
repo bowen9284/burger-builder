00a39600c68e6907b9f85b99d64d71752ca83e44
Matt Bowen <bowen9284@gmail.com>
2020-06-28 11:50:19 -0400

    adding tests for a container, a component and a reducer

---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,36 @@
+import reducer from './auth';
+import * as actionTypes from '../actions/actionTypes';
+import { bindActionCreators } from 'redux';
+
+describe('auth reducer', () => {
+  let initialState;
+  beforeEach(() => {
+    initialState = {
+      token: null,
+      userId: null,
+      error: null,
+      loading: false,
+      authRedirect: '/',
+    };
+  });
+  
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  it('should store the token upon login', () => {
+    expect(
+      reducer(initialState, {
+        type: actionTypes.AUTH_SUCCESS,
+        idToken: 'token',
+        userId: 'userId',
+      })
+    ).toEqual({
+      token: 'token',
+      userId: 'userId',
+      error: null,
+      loading: false,
+      authRedirect: '/',
+    });
+  });
+});
